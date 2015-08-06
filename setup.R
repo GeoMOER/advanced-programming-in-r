cefd6f077427ea6698b0d9bf2817cdccb7cfd290
fdetsch <flowla88@gmail.com>
2015-08-06 10:59:37 +0200

    R script to set up Gitbook software locally and initialize a new Gitbook.

---
+++ setup.R
@@ -0,0 +1,16 @@
+## if required, install 'Rgitbook' package
+# library(devtools)
+# install_github("raulossada/Rgitbook")
+library(Rgitbook)
+
+## check for local installation of Gitbook
+checkForGitbook()
+
+## check for newer version of Gitbook and, if required, install
+log_avl <- gitbookInfo()
+if (log_avl[2] != log_avl[1])
+  installGitbook()
+
+## create new Gitbook
+newGitbook("advancedProgrammingInR")
+initGitbook("advancedProgrammingInR/")
